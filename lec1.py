cacd54e35b34f22544895f7a23007eb98c42fe1b
ravi <ravidhawan8899@gmail.com>
2024-08-25 00:16:40 +0530

    basics of strings

--- lec1.py
+++ lec1.py
@@ -1,2 +1,8 @@
 # write any python Code 
-
+name = 'my name is john'   # strings in python are immutable(can't change)
+name[3]
+print(name)
+name[-4]   # it will print the 4th char form the last
+name[0:5]   #it will print the first 4 value
+name[1:]
+len(name)
